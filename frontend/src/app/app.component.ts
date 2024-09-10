2edf8f034ba11f96cb7c4c2d64495a80e8bd50a9
Maximilian Niessl <contact@maxniessl.com>
2024-09-10 09:41:07 +0200

    Added basic navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
+// components
+import { NavbarComponent } from "./navbar/navbar.component";
+
 @Component({
     selector: "app-root",
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NavbarComponent],
     templateUrl: "./app.component.html",
     styleUrl: "./app.component.css",
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+// components
+import { HomeComponent } from "./home/home.component";
+import { TabsComponent } from "./tabs/tabs.component";
+import { MusicComponent } from "./music/music.component";
+import { FaqComponent } from "./faq/faq.component";
+
+export const routes: Routes = [
+    {
+        title: "Home | Max Niessl",
+        path: "",
+        component: HomeComponent,
+    },
+    {
+        title: "Tabs | Max Niessl",
+        path: "tabs",
+        component: TabsComponent,
+    },
+    {
+        title: "Music | Max Niessl",
+        path: "music",
+        component: MusicComponent,
+    },
+    {
+        title: "FAQ | Max Niessl",
+        path: "faq",
+        component: FaqComponent,
+    },
+];
