78f430a0af4b83df43e9a3deeea8ec1f243d17fc
Maximilian Niessl <contact@maxniessl.com>
2024-09-10 23:20:10 +0200

    Added proper theme initialization

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+// basic
+import { APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
-
 import { routes } from "./app.routes";
 
+// services
+import { ThemeService } from "./services/app/theme.service";
+
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+    providers: [
+        provideZoneChangeDetection({ eventCoalescing: true }),
+        provideRouter(routes),
+        ThemeService,
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initializeTheme,
+            deps: [ThemeService],
+            multi: true,
+        },
+    ],
 };
+
+export function initializeTheme(themeService: ThemeService) {
+    return (): void => {
+        themeService.initTheme();
+    };
+}
--- frontend/src/app/services/app/theme.service.ts
+++ frontend/src/app/services/app/theme.service.ts
@@ -10,7 +10,17 @@ export class ThemeService {
         return localStorage.getItem("darkMode") === "true";
     }
 
+    initTheme() {
+        if (this.isDarkMode()) {
+            document.body.classList.add("dark");
+        }
+    }
+
     toggleDarkMode() {
+        document.body.classList.add("transition");
+        document.body.classList.add("duration-300");
+        document.body.classList.add("ease-in-out");
+
         if (this.isDarkMode()) {
             document.body.classList.remove("dark");
             localStorage.removeItem("darkMode");
