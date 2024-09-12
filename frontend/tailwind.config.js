3cfcc0a8565e89645248d24e5a778e91d2cc6fc2
Maximilian Niessl <contact@maxniessl.com>
2024-09-12 12:05:15 +0200

    Added title text

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{html,ts}"],
     theme: {
-        extend: {},
+        extend: {
+            keyframes: {
+                "slide-up": {
+                    "0%": { transform: "translateY(33%)", opacity: 0, filter: "blur(5px)" },
+                    "100%": { transform: "translateY(0)", opacity: 1 },
+                },
+            },
+            animation: {
+                "slide-up": "slide-up 1.5s ease backwards",
+                "slide-up-d-0.5s": "slide-up 1.5s ease 0.5s backwards",
+            },
+        },
         container: {
             center: true,
         },
