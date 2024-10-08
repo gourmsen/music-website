b7f820682e25957d364e8f973c5a3c70dd2d2786
Maximilian Niessl <contact@maxniessl.com>
2024-10-08 20:36:59 +0200

    Added basic login card

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,11 +1,46 @@
 // basic
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+// forms
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+// icons
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: "app-login",
     standalone: true,
-    imports: [],
+    imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule],
     templateUrl: "./login.component.html",
     styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+    loginForm: FormGroup;
+
+    passwordVisible = false;
+    passwordIcon = faEye;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            email: ["", [Validators.required, Validators.email]],
+            password: ["", Validators.required],
+        });
+    }
+
+    get email() {
+        return this.loginForm.get("email");
+    }
+
+    get password() {
+        return this.loginForm.get("password");
+    }
+
+    togglePasswordVisibility() {
+        this.passwordVisible = !this.passwordVisible;
+        this.passwordIcon = this.passwordVisible ? faEyeSlash : faEye;
+    }
+}
