e96a7bf4429bef069d5fd84a4cec9c1d107a37f2
Maximilian Niessl <contact@maxniessl.com>
2025-04-12 12:35:41 +0200

    Added song search

--- frontend/src/app/songs/songs.component.ts
+++ frontend/src/app/songs/songs.component.ts
@@ -1,27 +1,69 @@
 // basic
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { ToastComponent } from "../shared/toast/toast.component";
+
+// forms
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 // services
 import { SongService } from "../services/http/song.service";
 
+// icons
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 // interfaces
 import { SongListResponse } from "../interfaces/song";
 
+// enums
+import { ToastTypes } from "../shared/toast/toast.enums";
+
 @Component({
     selector: "app-songs",
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule, ToastComponent],
     templateUrl: "./songs.component.html",
     styleUrl: "./songs.component.css",
 })
 export class SongsComponent {
+    songsLoaded = false;
+
+    songs: any;
     songsSortedByArtist: any;
+
+    searchForm: FormGroup;
+    searchIcon = faMagnifyingGlass;
+
+    toastVisible: boolean;
+    toastType: ToastTypes;
+    toastMessage: string;
+    toastDuration: number;
 
     songListResponse: SongListResponse;
 
-    constructor(private songService: SongService) {}
+    constructor(private formBuilder: FormBuilder, private songService: SongService) {}
 
     ngOnInit() {
+        this.toastVisible = false;
+
+        this.searchForm = this.formBuilder.group({
+            search: ["", [Validators.required]],
+        });
+
+        this.search?.valueChanges.subscribe(() => {
+            this.filterSongs();
+        });
+
+        this.toastVisible = false;
+
+        this.toastType = ToastTypes.Info;
+        this.toastMessage = "Loading songs...";
+        this.toastDuration = -1;
+
+        setTimeout(() => {
+            this.toastVisible = true;
+        }, 10);
+
         this.songService.list().subscribe({
             next: (response) => {
                 this.songListResponse = response.body!;
@@ -39,9 +81,54 @@ export class SongsComponent {
 
                     return a.track_number - b.track_number;
                 });
+
+                this.songs = this.songsSortedByArtist;
+
+                this.toastVisible = false;
+                this.songsLoaded = true;
             },
-            error: (error) => {},
+            error: (error) => {
+                this.toastVisible = false;
+
+                this.toastType = ToastTypes.Danger;
+                this.toastMessage = "Unable to retrieve songs";
+                this.toastDuration = 5000;
+
+                setTimeout(() => {
+                    this.toastVisible = true;
+                }, 10);
+
+                this.songsLoaded = false;
+            },
             complete: () => {},
         });
+    }
+
+    get search() {
+        return this.searchForm.get("search");
+    }
+
+    filterSongs() {
+        let searchValue = this.search?.value.toLowerCase().trim();
+
+        if (!searchValue) {
+            this.songs = this.songsSortedByArtist;
+        } else {
+            this.songs = this.songsSortedByArtist.filter((song: any) => {
+                return (
+                    song.artist.toLowerCase().includes(searchValue) ||
+                    song.title.toLowerCase().includes(searchValue) ||
+                    song.album.toLowerCase().includes(searchValue)
+                );
+            });
+        }
+    }
+
+    onSongClick(songId: number) {
+        // TODO: add logic to handle song click
+    }
+
+    onToastClosed() {
+        this.toastVisible = false;
     }
 }
