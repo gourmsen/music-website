17eed796f30fccd3ecc9d30ff7586ab1b2217bb3
Maximilian Niessl <contact@maxniessl.com>
2026-02-15 23:30:45 +0100

    Add environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://emejbvddvwdnvwlcvkts.supabase.co',
+    supabaseKey: 'sb_publishable_rPQPPeQpTx8NuqoYLB3Zyw__TFDRkzs',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    supabaseUrl: 'https://emejbvddvwdnvwlcvkts.supabase.co',
+    supabaseKey: 'sb_publishable_rPQPPeQpTx8NuqoYLB3Zyw__TFDRkzs',
+};
